258dbffafa6c3d55fe62712cb01e1ac8b96f92a5
Hwangyounha <ipi0062@naver.com>
2022-12-11 17:19:25 +0900

    hospital 모델 변경

--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
+from hospital.models import Hospital
 # Create your views here.
+
+@api_view(['POST'])
+def signup(request):
+    hospital_id = request.data['hospital_id']
+    hospital_password = request.data['hospital_password']
+
+    user = User.objects.create_user(username=hospital_id, password=hospital_password)
+    token = Token.objects.create(user=user) # 토근 생성
+    user.save() # AUTH_USER_MODEL의 User 저장
+    
+    hospital_name = request.data['hospital_name']
+    hospital_birth = request.data['hospital_address']
+    hospital_phone_number = request.data['hospital_phone_number']
+    hospital_department = request.data['hospital_department']
+    
+    patient = Hospital(author = user,
+                      hospital_name = hospital_name,
+                      hospital_birth = hospital_birth, 
+                      hospital_phone_number = hospital_phone_number,
+                      hospital_department = hospital_department)
+    patient.save()
+
+    return Response(token.key, status=status.HTTP_201_CREATED)
