818e6724f59f2acdde5bdbcad388056df4352fb9
Hwangyounha <ipi0062@naver.com>
2022-12-29 17:54:31 +0900

    [FEAT] 메인화면면 함수 추가

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from reservation.views import reservation, reservation_cancel, hospital_get_delete_reservation
+from reservation.views import reservation, reservation_cancel, hospital_get_delete_reservation, reservation_search
 
 urlpatterns = [
     path('', hospital_get_delete_reservation),
     path('reservation/', reservation),
     path('cancel/', reservation_cancel),
+    path('reservation_search/', reservation_search)
 ]
